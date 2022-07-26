1c6257feb8663ac974ad778f9fef7e6e851253d2
Diego Allies <diegoallies27@gmail.com>
2022-07-26 12:29:12 +0200

    routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsa fugiat fuga libero, harum nihil corrupti aliquam ratione, quia explicabo maxime eos nisi iusto? Temporibus ut repellat inventore eaque pariatur?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsa fugiat fuga libero, harum nihil corrupti aliquam ratione, quia explicabo maxime eos nisi iusto? Temporibus ut repellat inventore eaque pariatur?</p>
+        </div>
+     );
+}
+ 
+export default About;
