30f0264658c22ffd90eb51964d469525ffecedcf
Diego Allies <diegoallies27@gmail.com>
2022-07-26 13:36:42 +0200

    added images and meta data

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,20 @@
+import Head from 'next/head'
+
 const About = () => {
-    return ( 
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsa fugiat fuga libero, harum nihil corrupti aliquam ratione, quia explicabo maxime eos nisi iusto? Temporibus ut repellat inventore eaque pariatur?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsa fugiat fuga libero, harum nihil corrupti aliquam ratione, quia explicabo maxime eos nisi iusto? Temporibus ut repellat inventore eaque pariatur?</p>
-        </div>
-     );
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      </div>
+    </>
+  );
 }
  
 export default About;
