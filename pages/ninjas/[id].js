87ea83f83d12e05dd170a3ab12faebfe1911fb20
Diego Allies <diegoallies27@gmail.com>
2022-07-26 14:06:23 +0200

    static paths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,21 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.map(ninja => {
+      return {
+        params: { id: ninja.id.toString() }
+      }
+    })
+  
+    return {
+      paths,
+      fallback: false
+    }
+  }
+  
+  const Details = () => {
     return (
       <div>
         <h1>Details Page</h1>
