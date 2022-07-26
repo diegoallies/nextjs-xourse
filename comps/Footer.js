abb414d62a08b4edfc8f0d7dfe5b3ff7d9e5ba6d
Diego Allies <diegoallies27@gmail.com>
2022-07-26 13:23:06 +0200

    addedvstyling

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return (
-      <footer>
-        Copyright 2021 Ninja List
-      </footer>
-    );
-  }
-   
-  export default Footer;
+  return (
+    <footer>
+      Copyright 2021 Ninja List
+    </footer>
+  );
+}
+ 
+export default Footer;
