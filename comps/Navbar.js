9d030f85767f47188a50e0471f6c71d94a3bf8ea
Diego Allies <diegoallies27@gmail.com>
2022-07-26 12:57:39 +0200

    nav

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,14 +1,16 @@
+import Link from 'next/link';
+
 const Navbar = () => {
-    return (
-      <nav>
-        <div className="logo">
-          <h1>Ninja List</h1>
-        </div>
-        <a>Home</a>
-        <a>About</a>
-        <a>Ninja Listing</a>
-      </nav>
-  );
-  }
-   
-  export default Navbar;
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Ninja List</h1>
+      </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/ninjas/"><a>Ninja Listing</a></Link>
+    </nav>
+);
+}
+ 
+export default Navbar;
