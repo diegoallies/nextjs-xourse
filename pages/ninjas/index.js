c437f79b1b9934f2b04ec0f1884d2edf0a46e8ce
Diego Allies <diegoallies27@gmail.com>
2022-07-26 13:59:37 +0200

    added single pager

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,29 @@
-const Ninjas = () => {
-    return (
-      <div>
-        <h1>All Ninjas</h1>
-      </div>
-    );
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
   }
-   
-  export default Ninjas;
+}
+
+const Ninjas = ({ ninjas }) => {
+  console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
+export default Ninjas;
