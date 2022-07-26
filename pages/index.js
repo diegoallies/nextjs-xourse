63c292c8bbe63d9c0dc71a7de7a14f6bb2dfea39
Diego Allies <diegoallies27@gmail.com>
2022-07-26 12:37:34 +0200

    added nav and foot

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-   <div>
-    <h1>Homepage</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ipsa fugiat fuga libero, harum nihil corrupti aliquam ratione, quia explicabo maxime eos nisi iusto? Temporibus ut repellat inventore eaque pariatur?</p>
-   </div>
+    <div>
+      <Navbar />
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <Footer />
+    </div>
   )
 }
